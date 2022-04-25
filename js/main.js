16938a3cd2b0351b603e4a27f9e5d15a4c92ab4f
Elandir <krzysztofn@student.agh.edu.pl>
2022-04-25 21:40:06 +0200

    Obracanie kostki: po kliknięciu na przycisk kostka obraca się na odpowwiedni bok.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
+//bok front - Cześć jak masz na imię?
 
-function animacja() {
+function letsIntroduce() {
+
+
+
+}
+
+
+
+////bok back Gra w zgadywanie
+//
+//function letsPlay() {
+//
+//}
+//
+////bok left
+//
+////bok right
+//
+////bok top
+//
+////bok bottom
+//
+//animacje oobracania kostki
+
+/*
+animacjaFront: X+0 Y+0  Z+0
+animacjaRigth: X+0 Y-90 Z+0
+animacjaLeft: X+0 Y+90 Z+0
+animacjaTop: X-90 Y+0 Z+0
+animacjaBottom: X+90 Y+0 Z+0
+animacjaBack: X-180 Y+0 Z+180
+
+[0, 0, 0, 0, -90, 0, 0, 90, 0, -90, 0, 0, 90, 0, 0, -180, 0, 180]
+*/
+
+function animacja(x, y, z) {
 const kostka = document.getElementById('kostka');
-
-const randomCoordinate = Math.random()*100;
-
-const coordinate = randomCoordinate > 50 ? 'X' : 'Y';
-
-const randomWaye = Math.random()*100;
-
-const whichWay  = randomWaye > 50 ? '-' : '+';
-
-const wKtoraStrone = 'rotate' + coordinate + '(' + whichWay + '90deg)';
 
 const obracanieKostki = new KeyframeEffect(
     kostka, // element to animate
 
      [
-       { transform: wKtoraStrone}
+       { transform: x + " " + y + " " + z } // 'rotateX(+90deg) rotateY(+0deg) rotateZ(+0deg)'
      ],
      { duration: 3000, fill: 'forwards' } // keyframe options
    );
@@ -25,3 +51,54 @@ const obracanieKostki = new KeyframeEffect(
 
  animacjaKostki.play();
   }
+
+function animacjaFront() {
+
+const x = 'rotateX(+0deg)';
+const y = 'rotateY(+0deg)';
+const z = 'rotateZ(+0deg)';
+
+animacja(x, y, z);
+}
+
+function animacjaBack() {
+  const x = 'rotateX(180deg)';
+  const y = 'rotateY(+0deg)';
+  const z = 'rotateZ(+180deg)';
+
+  animacja(x, y, z);
+}
+
+function animacjaLeft() {
+  const x = 'rotateX(+0deg)';
+  const y = 'rotateY(+90deg)';
+  const z = 'rotateZ(+0deg)';
+
+  animacja(x, y, z);
+}
+
+function animacjaRigth() {
+
+const x = 'rotateX(+0deg)';
+const y = 'rotateY(-90deg)';
+const z = 'rotateZ(+0deg)';
+
+animacja(x, y, z);
+}
+
+function animacjaTop() {
+  const x = 'rotateX(-90deg)';
+  const y = 'rotateY(+0deg)';
+  const z = 'rotateZ(+0deg)';
+
+  animacja(x, y, z);
+}
+
+function animacjaBottom() {
+  const x = 'rotateX(+90deg)';
+  const y = 'rotateY(+0deg)';
+  const z = 'rotateZ(+0deg)';
+
+  animacja(x, y, z);
+}
+
